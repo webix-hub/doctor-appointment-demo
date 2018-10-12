7bd7d333e68857197d0c12abf346e50deba29a5a
Maksim Kozhukh <mkozhukh@ya.ru>
2018-10-12 19:31:01 +0300

    [dev] correct name for deploy target

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ PACKAGE=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1).zip
 zip -rq ${PACKAGE} index.html ./data ./codebase
 
 aws s3 cp --quiet ./${PACKAGE} s3://webix-temp/${PACKAGE}
-curl -s "http://webix.io:2280/h00ks/deploy-preview?token=${PREVIEW_HOOK_TOKEN}&file=${PACKAGE}&source=webix&component=bank-app-demo&branch=${DRONE_BRANCH}"
+curl -s "http://webix.io:2280/h00ks/deploy-preview?token=${PREVIEW_HOOK_TOKEN}&file=${PACKAGE}&source=webix&component=doctor-appointment-demo&branch=${DRONE_BRANCH}"
