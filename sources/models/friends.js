b0aa3e634127e9d0bdc72b707ad1d3e33997e4bb
Nastya <akovalenko@xbsoftware.com>
2018-10-30 14:24:17 +0300

    [add] friends panel

---
+++ sources/models/friends.js
@@ -0,0 +1,16 @@
+export function getFriends(){
+	return friends;
+}
+
+const friends = [
+	{status:"online",photo:"heike",name:"Lyle Bauer",field:"Immunologist"},
+	{status:"offline",photo:"dottie",name:"Dorothy Walter",field:"Cardiologist"},
+	{status:"online",photo:"normand",name:"Phillip Hartman",field:"Dermatologist"},
+	{status:"online",photo:"lorena",name:"Josephine Prince",field:"Endocrinologist"},
+	{status:"offline",photo:"tommie",name:"Derek Mullen",field:"Family Physician"},
+	{status:"offline",photo:"tesha",name:"Lucy Mann",field:"Gastroenterologist"},
+	{status:"online",photo:"jesse",name:"Phillip Lambert",field:"Hematologist"},
+	{status:"online",photo:"marcia",name:"Marcia Lane",field:"Medical Geneticist"},
+	{status:"offline",photo:"abdul",name:"Landon Peterson",field:"Nephrologist"},
+	{status:"offline",photo:"lacy",name:"Angie Wilcox",field:"Neurologist"}
+];
--- sources/views/friends.js
+++ sources/views/friends.js
@@ -1,9 +1,32 @@
 import {JetView} from "webix-jet";
+import {getFriends} from "models/friends";
 
 export default class FriendsView extends JetView {
 	config(){
 		return {
-			template:"Friends"
+			rows:[
+				{ template:"Friends", type:"header", localId:"header" },
+				{
+					view:"list", css:"persons_list", select:true,
+					localId:"list",
+					type:{
+						height:65,
+						width:"auto",
+						template:obj => {
+							return `<image class="doctorphoto" src="data/photos/${obj.photo}_1.jpg" />
+							<div class="text">
+						  		<span class="doctorname">${obj.name}</span>
+								<span class="field">${(obj.field)}</span>
+								<span class="webix_icon mdi mdi-circle status ${obj.status}"></span>
+							</div>`
+						}
+					}
+				}
+			]
 		};
+	}
+	init(){
+		this.$$("list").parse(getFriends());
+		this.$$("header").setHTML(`Friends (${this.$$("list").count()})`);
 	}
 }
