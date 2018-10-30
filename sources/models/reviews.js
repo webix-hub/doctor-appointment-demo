d107b1d9fb87348ddd9f03b276d903b255e81b0c
Nastya <akovalenko@xbsoftware.com>
2018-10-30 13:43:04 +0300

    [add] reviews with stars

---
+++ sources/models/reviews.js
@@ -0,0 +1,11 @@
+export function getReviews(){
+	return reviews;
+}
+
+const reviews = [
+	{ id:1, value:"Punctuality", stars:5 },
+	{ id:2, value:"Manners", stars:4 },
+	{ id:3, value:"Knowledge", stars:5 },
+	{ id:4, value:"Cost", stars:4 },
+	{ id:5, value:"Colleagues", stars:4 }
+];
--- sources/views/reviews.js
+++ sources/views/reviews.js
@@ -1,7 +1,31 @@
 import {JetView} from "webix-jet";
+import {getReviews} from "models/reviews";
 
 export default class ReviewsView extends JetView {
 	config(){
-		return { template:"review with stars" };
+		return {
+			rows:[
+				{ template:"Reviews", type:"header"},
+				{
+					view:"dataview", xCount:1, select:true,
+					css:"reviews", localId:"reviews",
+					type:{
+						width:"auto", type:"tiles", height:80,
+						template:obj => {
+							let result = "";
+							for (let i = 1; i <= 5; i++){
+								let color = (i <= obj.stars) ? "gold" : "grey";
+								result += `<span class='webix_icon mdi mdi-star star ${color} ${i}'></span>`;
+							}
+							return `<span class="criterion">${obj.value}</span>
+							<span class="stars">${result}</span>`;
+						}
+					}
+				}
+			]
+		};
+	}
+	init(){
+		this.$$("reviews").parse(getReviews());
 	}
 }
